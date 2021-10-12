e4f7b9988d9d7ee686fdf39ae58c8f7dd1b3a709
dumpserver <dumpserver@hotmail.com>
2021-10-12 05:01:58 -0300

    Create ScrollView and BigText Component.

--- components/BigText.tsx
+++ components/BigText.tsx
@@ -3,7 +3,7 @@ import {Text} from 'react-native'
 
 const BigText = () => {
   return (
-    <Text style={{fontSize: 92}}>
+    <Text style={{fontSize: 60}}>
       Zero um um um zero zero um um 
       Gritando zeros e ouvindo um um uns
       Corte minha cambalhota, assine meu salto de costas
--- components/SimpleScroll.tsx
+++ components/SimpleScroll.tsx
@@ -3,7 +3,7 @@ import { ScrollView, Text } from 'react-native'
 
 const SimpleScroll = () => (
     <ScrollView>
-     <Text style={{fontSize: 92}}>
+     <Text style={{fontSize: 60}}>
         Zero um um um zero zero um um 
         Gritando zeros e ouvindo um um uns
         Corte minha cambalhota, assine meu salto de costas
